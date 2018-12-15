82779a9b62629a1bbffadaed23a7b0079f42800e
JohnS8631 <johns8631@yahoo.com>
2018-12-15 13:53:45 -0500

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+ $link = mysqli_connect("localhost", "doubl123_johnnybg", "guppy11", "doubl123_secretdi");
+        
+        if(mysqli_connect_error()) {
+            
+            
+            die ("Database Connection Error");
+            
+        }
+        
+?>
